ba65beaa4d73665732014314d3eb4ba537f137cb
ZaitsevD12 <70914389+ZaitsevD12@users.noreply.github.com>
2020-09-16 13:10:44 +0300

    L2

---
+++ Рисунок Lampanel.asm
@@ -0,0 +1,37 @@
+
+
+mov 150,r0
+
+out r0,p0
+
+mov 1BC,r1
+
+out r1,p1
+
+mov 150,r2
+
+out r2,p2
+
+mov 100,r3
+
+out r3,p3
+
+mov  FFFF,r0
+
+out r0,p4
+
+mov 7B6E,r1
+
+out r1,p5
+
+mov 36DC,r2
+
+out r2,p6
+
+mov 1FF8,r3
+
+out r3,p7
+
+m:
+mov 1,r1
+stop
